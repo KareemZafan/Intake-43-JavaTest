4390864c645d302e71c5c6fd830576fd52fc79da
Raghda-Emad <raghdamohammademad@gmail.com>
2023-02-12 14:44:27 +0200

    apply all review comments

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println( "Hello Intake-43" );
 
         // list of students' names
-        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader","Raghda" ) );
+        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader", "Raghda" ) );
 
         for (String name : names) {
             System.out.println( name );
