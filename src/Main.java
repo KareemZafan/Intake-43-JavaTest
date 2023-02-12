c0ae44450dadde869b92e3c319722f8d2bc81acb
KareemZafan <kareemm.zafan@gmail.com>
2023-02-12 15:23:14 +0200

    Adding Nada

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println( "Hello Intake-43" );
 
         // list of students' namess
-        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader", "Raghda" ,"Kirolos","Ahmed Arthur", "Abdullah" ) );
+        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader", "Raghda" ,"Kirolos","Ahmed Arthur", "Abdullah", "Nada" ) );
         for (String name : names) {
             System.out.println( name );
         }
