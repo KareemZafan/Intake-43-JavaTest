585ac5542cd9850c417b0437d2b6651e5bfab47f
Ahmed Mahmoud <Ahmedwow19@gmail.com>
2023-02-12 14:53:07 +0200

    added my name to the list

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println( "Hello Intake-43" );
 
         // list of students' names
-        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader" ,"Kirolos") );
+        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader" ,"Kirolos","Ahmed Arthur") );
 
         for (String name : names) {
             System.out.println( name );
