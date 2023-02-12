e14cc1be64255ccba1bbf4066a0b1e5b9e257a70
KareemZafan <kareemm.zafan@gmail.com>
2023-02-12 14:13:51 +0200

    Create new Java repo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( "Hello Intake-43" );
+
+        // list of students' names
+        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader" ) );
+
+        for (String name : names) {
+            System.out.println( name );
+        }
+    }
+}
