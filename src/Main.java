636bce8219c2ae32949e3bef0d9e69f49d685178
KareemZafan <kareemm.zafan@gmail.com>
2023-02-12 15:10:16 +0200

    add Ashraf in branch A

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         System.out.println( "Hello Intake-43" );
 
         // list of students' names
-        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader" ,"Kirolos","Ahmed Arthur") );
+        List<String> names = new ArrayList<>( List.of( "Kareem", "Fouda", "Nader" ,"Kirolos","Ahmed Arthur","Ashraf") );
 
         for (String name : names) {
             System.out.println( name );
